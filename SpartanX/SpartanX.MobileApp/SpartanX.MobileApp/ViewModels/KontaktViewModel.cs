2c60030a769dab12f7524b140a42bdca3fb80736
6en6ar <73951749+6en6ar@users.noreply.github.com>
2021-10-05 13:01:34 +0200

    kontakt page

---
+++ SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/ViewModels/KontaktViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpartanX.MobileApp.ViewModels
+{
+    public class KontaktViewModel : BaseViewModel
+    {
+        public KontaktViewModel()
+        {
+            Title = "Kontakt page!";
+        }
+    }
+}
