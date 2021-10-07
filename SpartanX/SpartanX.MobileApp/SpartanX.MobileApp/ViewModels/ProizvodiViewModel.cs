2616357f5007c78f6a082d2eeb7a07e73a4e1773
6en6ar <73951749+6en6ar@users.noreply.github.com>
2021-10-07 11:59:10 +0200

    izlistavanje proizvoda na mobilnoj

---
+++ SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/ViewModels/ProizvodiViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SpartanX.MobileApp.ViewModels
+{
+    public class ProizvodiViewModel
+    {
+        public ProizvodiViewModel()
+        {
+            komanda = new Command(async () => await Init());
+        }
+        private readonly APIService _service = new APIService("Proizvodi");
+        public ObservableCollection<ModelSpartanX.Proizvodi> ListaProizvoda { get; set; } = new ObservableCollection<ModelSpartanX.Proizvodi>();
+        public ICommand komanda { get; set; }
+        public async Task Init()
+        {
+            var lista = await _service.Get<IEnumerable<ModelSpartanX.Proizvodi>>(null);
+            //prvo cistimo proizvode prije refresha
+            ListaProizvoda.Clear();
+            foreach (var item in lista)
+            {
+                ListaProizvoda.Add(item);
+            }
+        }
+        
+    }
+}
--- SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/Views/ProizvodiPage.xaml.cs
+++ SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/Views/ProizvodiPage.xaml.cs
@@ -4,19 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SpartanX.MobileApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class ProizvodiPage : ContentPage
     {
-        public LoginPage()
+        private ProizvodiViewModel model;
+        public ProizvodiPage()
         {
             InitializeComponent();
-            this.BindingContext = new LoginPageViewModel();
+            BindingContext = model = new ProizvodiViewModel();
         }
-        
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await model.Init();
+        }
     }
+   
 }
