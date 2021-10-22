e569652350d83c2356c59ae44a2912abe6c58969
6en6ar <73951749+6en6ar@users.noreply.github.com>
2021-10-22 16:09:30 +0200

    seeding 1

---
+++ SpartanX/SpartanX/SeedSetup/SetupService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpartanX.SeedSetup
+{
+    public class SetupService
+    {
+        public void Init(SpartanX.Database.SpartanXContext context)
+        {
+            //context.Database.Migrate();
+            //dodavanje podataka
+        }
+    }
+}
