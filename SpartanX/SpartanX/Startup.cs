6d34823a7f791727d8cbe023600a9f2a8623efca
6en6ar <73951749+6en6ar@users.noreply.github.com>
2021-11-30 10:52:17 +0100

    swagger

--- SpartanX/SpartanX/Startup.cs
+++ SpartanX/SpartanX/Startup.cs
@@ -86,6 +86,7 @@ namespace SpartanX
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
 
             //app.UseHttpsRedirection();
