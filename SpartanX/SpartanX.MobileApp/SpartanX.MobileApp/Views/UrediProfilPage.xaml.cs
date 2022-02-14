d0a86ea512753dd0862adcd0a451fe01efa65d4f
6en6ar <73951749+6en6ar@users.noreply.github.com>
2022-02-14 09:17:58 +0100

    dodan check na update

--- SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/Views/UrediProfilPage.xaml.cs
+++ SpartanX/SpartanX.MobileApp/SpartanX.MobileApp/Views/UrediProfilPage.xaml.cs
@@ -13,10 +13,49 @@ namespace SpartanX.MobileApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UrediProfilPage : ContentPage
     {
+        APIService service = new APIService("Kupci");
+        public UrediProfilViewmodel model { get; set; }
         public UrediProfilPage()
         {
             InitializeComponent();
+            BindingContext = model = new UrediProfilViewmodel();
 
+        }
+
+        private async void Update_Clicked(object sender, EventArgs e)
+        {
+            List<ModelSpartanX.Kupci> lista = await service.Get<List<ModelSpartanX.Kupci>>(null);
+            bool vecPostojiIme = false;
+            bool vecPostojiMail = false;
+            foreach (var item in lista)
+            {
+                if (item.KorisnickoIme.Equals(inputKorIme.Text) == true)
+                {
+                    vecPostojiIme = true;
+                }
+                if (item.Email.Equals(inputMail.Text) == true)
+                {
+                    vecPostojiMail = true;
+                }
+            }
+            if (vecPostojiIme)
+            {
+                await DisplayAlert("Greska", "Korisnik sa tim korisnickim imenom vec postoji", "OK");
+                KImeError.Text = "Korisnicko ime je vec registrovano!";
+                KImeError.IsVisible = true;
+            }
+            else if (vecPostojiMail)
+            {
+                await DisplayAlert("Greska", "Korisnik sa tim emailom vec postoji", "OK");
+                erroMail.Text = "Email vec registrovan!";
+                erroMail.IsVisible = true;
+
+            }
+            else
+            {
+
+                await model.Uredi();
+            }
         }
     }
 }
